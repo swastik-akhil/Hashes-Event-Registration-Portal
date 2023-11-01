85ce9dcf108fbe203515d05a18ac0aceca1b0f3c
swastik-akhil <akhilmindset@gmail.com>
2023-11-01 14:39:23 +0530

    Initialising files and folders and added userModel

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+require(dotenv).config();
+
+
+
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
+        
---
+++ model/userModel.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true, 
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true, 
+    lowercase: true, 
+  },
+  profile_photo_url: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (url) => {
+        const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+        return urlRegex.test(url);
+      },
+      message: 'Invalid URL format for profile photo.',
+    },
+  },
+  branch: {
+    type: String,
+    required: true,
+    enum: ['CSE', 'ECE', 'ME', 'EE', 'CE', 'IT', 'EN', 'CSE(DS)', 'CSE(AIML)', 'IT', 'CSIT', 'CIVIL', 'ECE' ], // Limit to specific options
+  },
+  year: {
+    type: String,
+    required: true,
+    enum: ['First', 'Second', 'Third', 'Fourth'], // Limit to specific options
+  },
+  student_number: {
+    type: String,
+    required: true,
+    unique: true, 
+  },
+  roll_number: {
+    type: String,
+    // required: true,
+    unique: true, 
+  },
+  payment_amount: {
+    type: Number,
+    required: true,
+    min: 500, 
+    max : 500
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
